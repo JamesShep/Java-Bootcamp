007115823953f05111a31697ab22bde015f74dc2
JamesShep <26717741+JamesShep@users.noreply.github.com>
2019-11-19 20:19:13 +1000

    Week 3.6 - For loops

---
+++ Week3/src/com/coderscampus/objects/Loops.java
@@ -0,0 +1,20 @@
+package com.coderscampus.objects;
+
+import java.awt.*;
+
+public class Loops {
+
+    public static void main(String [] args) {
+
+        System.out.println("Square roots");
+
+        //Initialisation; Termination condition; Increment
+        int a = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.print(i + ": ");
+            System.out.println(i*i);
+        }
+    }
+
+}
