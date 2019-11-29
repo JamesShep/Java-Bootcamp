436f2d22c31370541c8c8e736666ea01c9a587ae
JamesShep <26717741+JamesShep@users.noreply.github.com>
2019-11-30 05:19:01 +1000

    Slight changes - some 'bug' fixes. Addition of closing tag for scanner object setting of boolean variable for win/lose

--- Assignment2/src/com/coderscampus/Assignment2/NumberGuess.java
+++ Assignment2/src/com/coderscampus/Assignment2/NumberGuess.java
@@ -11,30 +11,27 @@ public class NumberGuess {
         Scanner input = new Scanner(System.in);
 
         //variables
-        final int MAX_NUMBER = 101;
-        final int MIN_NUMBER = 0;
+        final int MAX_NUMBER = 100;
+        final int MIN_NUMBER = 1;
         int numberOfGuesses = 5;
         int playerGuessNumber = 0;
         int randomNumber;
         int playerGuess;
+        boolean hasWon = false;
 
         //setup of random number object, and assigning max limit
         Random random = new Random();
-        randomNumber = random.nextInt(MAX_NUMBER);
+        randomNumber = random.nextInt(MAX_NUMBER) +1;
+
         //Below line was added for testing
-        //System.out.println(randomNumber);
+        System.out.println(randomNumber);
 
-        //Add +1 if random number is 0 (0 is not allowed for the assignment)
-        if (randomNumber == 0) {
-            randomNumber++;
-        }
-
-        //Start of while loop, looping to value set in numberOfGuesses (5)
+        //Start of while loop, looping to value set in numberOfGuesses (numberOfGuesses)
         while (playerGuessNumber < numberOfGuesses) {
-            System.out.println("Please pick a number between 1 and 100 (Inclusive)");
+            System.out.println("Please pick a number between 1 and 100");
             playerGuess = input.nextInt();
 
-            if (playerGuess <= MIN_NUMBER || playerGuess >= MAX_NUMBER) {
+            if (playerGuess < MIN_NUMBER || playerGuess > MAX_NUMBER) {
                 System.out.println("Your guess is not between 1 and 100, please try again");
             }
             else if (playerGuess > randomNumber) {
@@ -47,12 +44,15 @@ public class NumberGuess {
             }
             else {
                 System.out.println("Your guess of " + playerGuess + " is correct!");
+                System.out.println("It took you " + (playerGuessNumber + 1 ) + " guesses!");
+                hasWon = true;
                 break;
             }
         }
-        if (playerGuessNumber == (numberOfGuesses)) {
-            System.out.println("Your lose!");
+        if (!hasWon) {
+            System.out.println("You lose!");
             System.out.println("The number to guess was: " + randomNumber);
         }
+        input.close();
     }
 }
